58e6781fe7d2943d6a7996b08cdce2eba07dfecd
pedrocarneiro11 <pedrocarneirocunha87@gmail.com>
2022-06-28 13:46:14 -0300

    commit inicial

---
+++ br/dio/exercicios/arrays/consoantes.java
@@ -0,0 +1,35 @@
+package br.dio.exercicios.arrays;
+
+import java.util.Scanner;
+
+public class consoantes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int quantidadeConsoantes = 0, count = 0;
+
+        do {
+            System.out.println("Letra: ");
+            String letra = scan.next();
+
+            if ( !(letra.equalsIgnoreCase("a") |
+                    letra.equalsIgnoreCase("e") |
+                    letra.equalsIgnoreCase("i") |
+                    letra.equalsIgnoreCase("o") |
+                    letra.equalsIgnoreCase("u"))) {
+                consoantes[count] = letra;
+                quantidadeConsoantes++;
+            }
+            count++;
+        } while(count < consoantes.length);
+
+        System.out.println("Consoantes: ");
+        for ( String consoante : consoantes ) {
+            if ( consoante != null) {
+                System.out.println(consoante + " ");
+            }
+        }
+        System.out.println("Quantidades de consoantes: " + quantidadeConsoantes);
+    }
+}
